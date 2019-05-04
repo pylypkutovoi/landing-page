1fb7fe9eb02262614ddecbbc6ad0367d9e12aea6
pylypkutovoi <pylyp.kutov@gmail.com>
2019-05-04 21:59:12 +0200

    Добавил js для меню и переделал селектор в блоке offer

---
+++ js/main.js
@@ -0,0 +1,35 @@
+$(window).load(function() {
+
+  $(".before-after").twentytwenty({
+    before_label: 'без скинали',
+    after_label: 'со скинали'
+  });
+  $('.before-slider').slick({
+    draggable: false,
+    dots: true,
+    dotsClass: 'before-slider__dots'
+  });
+
+  $('.menu-button').on('click', function (e) {
+    $('.menu').toggleClass('menu--active')
+  })
+  
+  $("a[href^='#']").click(function name(params) {
+    var _href = $(this).attr("href");
+    $("html, body").animate({ scrollTop: $(_href).offset().top -100 + 'px'})
+  })
+
+  // Selector
+  
+	$('.form__group').on('click', function() {
+		$('.select__dropdown').toggleClass('select__dropdown--open');
+  });
+  
+  $('.select__option').on('click', function(){
+		var value = $(this).attr('data-value');
+		$('#select-type').val(value);
+		$('.select--checked').text(value);
+		$('.select__dropdown').toggleClass('select__dropdown--open');
+	});
+});
+
